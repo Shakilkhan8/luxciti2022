17bedaa643e076134d10d76f8ee94f359540812d
Shakilkhan8 <48030846+Shakilkhan8@users.noreply.github.com>
2022-09-21 12:29:09 -0400

    Add files via upload

--- odoo_laravel_conector/models/operations.py
+++ odoo_laravel_conector/models/operations.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import requests
-
+import base64
 from odoo import models, fields, api, _
 
 
@@ -13,12 +13,20 @@ class LaravelInheritProductBrand(models.Model):
     _inherit = "product.brand"
     unique_id = fields.Char(required=False)
 
+class LaravelInheritRespartner(models.Model):
+    _inherit = "res.partner"
+    unique_id = fields.Char(required=False)
+
+class LaravelInheritProductTemplate(models.Model):
+    _inherit = "product.template"
+    unique_id = fields.Char(required=False)
+
 
 class LaravelConnectorOperations(models.TransientModel):
     _name = 'laravel.connector.operations'
 
     name = fields.Selection(string="Select Operation",
-                            selection=[('p_category', 'Product Category'), ('p_brand', 'Product Brand'), ],
+                            selection=[('p_category', 'Product Category'), ('p_brand', 'Product Brand'),('product', 'Product'),('customer', 'Customer') ],
                             required=True, )
 
     def create_product_category(self, *data):
@@ -27,10 +35,34 @@ class LaravelConnectorOperations(models.TransientModel):
                 for rec in li:
                     unique_id = self.env['product.category'].search([]).mapped('unique_id')
                     if not rec['unique_id'] in unique_id:
-                        self.env['product.category'].create({
+                        category = self.env['product.category'].create({
                             'name': rec['name'],
                             'unique_id': rec['unique_id']
                         })
+                    if rec['subcatagory']:
+                        if category:
+                            parent_category_for_sub = category.id
+                        else:
+                            parent_category_for_sub = self.env['product.category'].search([('unique_id','=',rec['unique_id'])]).id
+                        for subcateg in rec['subcatagory']:
+                            if not subcateg['unique_id'] in unique_id:
+                                sub_category = self.env['product.category'].create({
+                                'name': subcateg['name'],
+                                'parent_id': parent_category_for_sub,
+                                'unique_id': subcateg['unique_id']
+                            })
+                            if subcateg['subtosub_catagory']:
+                                if sub_category:
+                                    parent_category_for_sub_sub = sub_category.id
+                                else:
+                                    parent_category_for_sub_sub = self.env['product.category'].search([('unique_id','=',subcateg['unique_id'])]).id
+                                for subsubcateg in subcateg['subtosub_catagory']:
+                                    if not subsubcateg['unique_id'] in unique_id:
+                                        sub_sub__category = self.env['product.category'].create({
+                                        'name': subsubcateg['name'],
+                                        'parent_id': parent_category_for_sub_sub ,
+                                        'unique_id': subsubcateg['unique_id']
+                                    })
 
     def create_product_brand(self, *data):
         if data:
@@ -42,6 +74,36 @@ class LaravelConnectorOperations(models.TransientModel):
                             'name': rec['name'],
                             'unique_id': rec['id']
                         })
+
+    def create_laravel_customers(self,*data):
+        if data:
+            for li in data:
+                for rec in li:
+                    # fff = self.env['res.partner'].browse(14)
+                    # img = rec['image_user']
+                    # b_img = requests.post(url=img).content
+                    unique_id = self.env['res.partner'].search([]).mapped('unique_id')
+                    if not str(rec['id']) in unique_id:
+                        self.env['res.partner'].create({
+                            'name': rec['name'],
+                            'unique_id': rec['id'],
+                            'phone': rec['phone'],
+                            'email': rec['email'],
+                            # 'image_1920': b_img,
+                        })
+
+    def create_laravel_products(self, *data):
+        if data:
+            for li in data:
+                for rec in li:
+                    unique_id = self.env['product.template'].search([]).mapped('unique_id')
+                    if not str(rec['id']) in unique_id:
+                        self.env['product.template'].create({
+                            'name': rec['title'],
+                            'unique_id': rec['id'],
+                            'barcode': rec['sku'],
+                        })
+
     def sync_action(self):
         for rec in self:
             if rec.name == 'p_category':
@@ -57,5 +119,21 @@ class LaravelConnectorOperations(models.TransientModel):
                     request_data = requests.post(url='https://mastbeauty.com/api/get-brand/')
                     collected_data = request_data.json().get('data')
                     rec.create_product_brand(collected_data)
+                except Exception as e:
+                    print(e)
+
+            if rec.name == 'customer':
+                try:
+                    request_data = requests.post(url='https://mastbeauty.com/api/get-users/')
+                    collected_data = request_data.json().get('data')
+                    rec.create_laravel_customers(collected_data)
+                except Exception as e:
+                    print(e)
+
+            if rec.name == 'product':
+                try:
+                    request_data = requests.post(url='https://mastbeauty.com/api/get-product/')
+                    collected_data = request_data.json().get('data')
+                    rec.create_laravel_products(collected_data)
                 except Exception as e:
                     print(e)
